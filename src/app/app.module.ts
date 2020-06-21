b05bb1d612a70cee95c4318c770754d1d98348a7
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-21 22:56:43 +0900

    [add]コンポーネントの切り分け開始

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TaskListComponent],
   imports: [
     BrowserModule,
     FormsModule],
