67f241aa17e9d3fc26bf6fdcd2fa2f2ad1498ca3
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-23 09:07:44 +0900

    [add]コンポーネントの作成

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  tasks = [
+    { title: '牛乳を買う', done: false, deadline: new Date('2021-01-01') },
+    { title: '可燃ゴミを出す', done: true, deadline: new Date('2020-01-02') },
+    { title: '銀行に行く', done: false, deadline: new Date('2020-01-03') }
+  ];
 
-  ngOnInit(): void {
+  newTask = {
+    title: '',
+    deadline: new Date()
+  };
+
+  ngOnInit(): void {}
+
+  addTask() {
+    this.tasks.push({
+      title: this.newTask.title,
+      done: false,
+      deadline: new Date(this.newTask.deadline)
+    });
+    this.newTask = {
+      title: '',
+      deadline: new Date()
+    };
   }
 
+  isOverdue(task) {
+    return !task.done && task.deadline < new Date().setHours(0, 0, 0, 0);
+  }
 }
