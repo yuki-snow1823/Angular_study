c3eb1fe0bdce1740f3990dff3e82d49ac9545637
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-26 10:35:14 +0900

    [add]超過期日の修正

--- src/app/task-list-item/task-list-item.component.ts
+++ src/app/task-list-item/task-list-item.component.ts
@@ -1,19 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { Task } from '../../models/task';
 @Component({
   selector: 'app-task-list-item',
   templateUrl: './task-list-item.component.html',
   styleUrls: ['./task-list-item.component.scss']
 })
 export class TaskListItemComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
-  @Input() task;
+  ngOnInit(): void { }
+  @Input() task: Task;
   /* TaskListComponent から TaskListItemComponent の 
   task クラス変数に対して、（自分のクラス変数である） task を渡す */
 
-  isOverdue(task) {
-    return !task.done && task.deadline < new Date().setHours(0, 0, 0, 0);
+  isOverdue(task: Task): boolean {
+    return !task.done && task.deadline && task.deadline.getTime() < (new Date()).setHours(0, 0, 0, 0);
   }
 }
