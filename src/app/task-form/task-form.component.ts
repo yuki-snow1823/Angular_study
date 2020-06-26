18520decae54424419525f6b0ccb7ca84e6300e6
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-26 10:35:31 +0900

    [add]超過期日が空欄でも大丈夫なように設定

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-
+import { Task } from '../../models/task';
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
@@ -8,24 +8,24 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class TaskFormComponent implements OnInit {
   constructor() {}
 
-  @Output() addTask = new EventEmitter(); // $eventを受け取る
+  @Output() addTask = new EventEmitter<Task>();
 
   newTask = {
     title: '',
-    deadline: new Date()
+    deadline: null
   };
 
   ngOnInit(): void {}
 
-  submit() {
-    this.addTask.emit({ // ここで発火
-      title: this.newTask.title,
-      done: false,
-      deadline: new Date(this.newTask.deadline)
-    });
+  submit(): void {
+   this.addTask.emit({
+     title: this.newTask.title,
+     done: false,
+     deadline: this.newTask.deadline ? new Date(this.newTask.deadline) : null,
+   });
     this.newTask = {
       title: '',
-      deadline: new Date()
+      deadline: null,
     };
   }
 }
